2e4954fae4d937d5660eacf3d1082dba682b734c
unknown <anivireddisaideepthi1995@gmail.com>
2020-03-24 11:57:51 +0530

    This is my first commit of my project

---
+++ JavaProjectPractice/src/calculator.java
@@ -0,0 +1,14 @@
+
+public class calculator {
+	public int Addition(int a, int b)
+
+	{
+		int c = a+b;
+		return c;
+	}
+
+	public void main(String[] args) {
+		calculator object1 = new calculator();
+		object1.Addition(23, 24);
+	}
+}
